7cd03fe24a1bf318f49f335ed9efbad6a663db7c
xiefei <410337505@qq.com>
2014-02-25 10:31:03 +0800

    test

---
+++ SKPhysicsSimulate01Tests/SKPhysicsSimulate01Tests.m
@@ -0,0 +1,36 @@
+//
+//  SKPhysicsSimulate01Tests.m
+//  SKPhysicsSimulate01Tests
+//
+//  Created by Kobe on 13-7-13.
+//  Copyright (c) 2013年 Kobe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SKPhysicsSimulate01Tests : XCTestCase
+
+@end
+
+@implementation SKPhysicsSimulate01Tests
+
+- (void)setUp
+{
+    [super setUp];
+    NSLog(@"");
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
